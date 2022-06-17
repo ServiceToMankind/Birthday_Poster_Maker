bf8ce9a919b6255baa002d7ce7ee996c5ae34b5b
kiranpranay <pranaybannu12@gmail.com>
2022-06-17 17:46:40 +0530

    Initial Ui BringUp
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from os import path
+
+
+
+def init_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
